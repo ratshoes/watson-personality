71d20d84ef76c7213efad09dc07c13a1711ff4a1
Greyson Malo <steven.magadan@hotmail.com>
2019-06-03 17:50:23 -0700

    got react frontend to display backend json users

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+  callApi() {
+    fetch("http://localhost:8000/api/getusers")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }))
+      .catch(err => err);
+  }
+
+  componentDidMount() {
+    this.callApi();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to react xD</h1>
+        </header>
+        <p className="App-intro">{this.state.apiResponse}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const path = require("path");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const app = express();
+const cors = require("cors");
 const apiRouter = require("./routes/api.js");
 
 const PORT = process.env.PORT || 8000;
@@ -19,6 +20,7 @@ mongoose
   .then(() => console.log("Mongo is now connected"))
   .catch(error => console.log(error));
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -26,6 +28,9 @@ app.use(bodyParser.json());
 // app.get("/", (req, res) => {
 //   res.json("Server is working on root directory");
 // });
+app.get("/", (req, res) => {
+  res.json({ message: "homepage root" });
+});
 app.use("/api", apiRouter);
 
 // app.use(require('./routes/web'))
